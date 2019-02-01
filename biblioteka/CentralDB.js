229178859d8bf600d736626e3e5ff485115b086a
Camisalo <konrad.gebczynski@gmail.com>
2019-02-01 19:39:46 +0100

    new

---
+++ biblioteka/CentralDB.js
@@ -0,0 +1,40 @@
+
+class CentralDB {
+    
+    getRecords(url) {
+        return new Promise((resolve, reject) => { 
+            var httpReq = new XMLHttpRequest(); 
+            httpReq.onreadystatechange = () => { 
+                if (httpReq.readyState === 4) { 
+                    if (httpReq.status === 200) { 
+                        resolve(JSON.parse(httpReq.responseText)); 
+                    } else { 
+                    reject(new Error(httpReq.statusText)); 
+                    } 
+                } 
+            }
+            httpReq.open("GET", url, true); 
+            httpReq.setRequestHeader('Content-Type','application/json');
+            httpReq.send(null);
+        });
+    }
+
+    insertRecords(data, url) {
+        return new Promise((resolve, reject) => { 
+            var httpReq = new XMLHttpRequest(); 
+            httpReq.onreadystatechange = () => { 
+                if (httpReq.readyState === 4 && httpReq.status === 200) {
+                    // console.log("WYSŁANO " + httpReq.statusText);
+                    resolve("OK"); 
+                } else { 
+                    // console.log(httpReq.statusText);
+                    // reject(new Error(httpReq.status)); 
+                } 
+                
+            }
+            httpReq.open("POST", url, true); 
+            httpReq.setRequestHeader('Content-Type','application/json');
+            httpReq.send(JSON.stringify(data));
+        });  
+    }
+}
